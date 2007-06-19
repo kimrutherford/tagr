60074f811b21667c6f4ee7289b23ae6ff237cf9c
kmr <kmr@f6704991-25e9-0310-81ae-dd6c47562deb>
2007-06-19 07:38:48 +0000

    Initial import of tagr.
    git-svn-id: file:///home/kmr44/Work/subversion_repository/tagr/trunk@413 f6704991-25e9-0310-81ae-dd6c47562deb

---
+++ lib/File/Tagr.pm
@@ -0,0 +1,26 @@
+package File::Tagr;
+
+=head1 NAME
+
+File::Tagr - flexibly tag files and their contents
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 FUNCTIONS
+
+=cut
+
+use warnings;
+use strict;
+use Carp;
+use vars qw($VERSION @ISA @EXPORT);
+use Exporter;
+
+@ISA = qw( Exporter );
+@EXPORT = qw( );
+
+$VERSION = '0.01';
